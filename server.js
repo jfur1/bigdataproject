ae201c6705a78ac22d36bb753662cb007529bc64
John Furlong <johnsdump1@gmail.com>
2020-04-18 13:18:32 -0600

    Optimization Math

--- server.js
+++ server.js
@@ -6,9 +6,58 @@ const passport = require('passport');
 const expressLayouts = require('express-ejs-layouts');
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcrypt');
+var cov = require( 'compute-covariance' );
+var unirest = require("unirest");
+const math = require('mathjs');
+
 const { ensureAuthenticated, forwardAuthenticated } = require('./resources/auth');
+var Portfolio = require('./resources/optimizer');
+
 
 const app = express();
+
+// let portfolio = new Portfolio('aapl', 'msft', 'nike', 1546448400, 1562086800, '1d', 0.02, 1000);
+// console.log(portfolio.period)
+
+// ------- Testing Area --------- //
+const alpha = 0.02;  // Requested return percentage
+const r = [0.024, 0.018, 0.009];
+const r_t = math.transpose(r);
+const e = [1, 1, 1];
+const e_t = math.transpose(e);
+const Q = [ [.0033, .00163, -.0007524], 
+            [.00163, .00183, -.000563], 
+            [-.0007524, -.000563, .001976] ];
+const Q_inv = math.inv(Q);
+console.log('Q Inverse: ', Q_inv);
+
+const x_star_1 = math.multiply(Q_inv, r);
+console.log("Chunk 1: ", x_star_1);
+
+const x_star_2 = math.multiply(Q_inv, e);
+console.log("Chunk 2: ", x_star_2);
+
+// ---- Solve for Lambdas ---- //
+//  [ [a, b],       [ [(r_t * Q_inv * r),  (r_t * Q_inv * e)],
+//     [b, d] ] =>    [(r_t * Q_inv * e),  (e_t * Q_inv * e) ] ] 
+const a = math.multiply(r_t, Q_inv, r);
+const b = math.multiply(r_t, Q_inv, e);
+const d = math.multiply(e_t, Q_inv, e);
+const A = [[a, b],
+            [b, d]];
+console.log("Matrix A: ", A);
+const b_vec = math.matrix([.02, 1]);
+console.log(b_vec.size())
+const lambdas = math.lusolve(A, b_vec);
+console.log("Lambda Values: ", lambdas);
+
+// ---- Substitute Lambdas to solve for x_star ----- //
+const new_chunk1 = x_star_1.map(function(x) {return x * lambdas[0]});
+const new_chunk2 = x_star_2.map(function(x) {return x * lambdas[1]});
+const x_star = math.add(new_chunk1, new_chunk2);
+console.log("Final Output Distribution: ", x_star);
+
+// ---------------------------- end ------------------------- //
 
 // Passport Config
 passport.use(
@@ -276,6 +325,43 @@ app.get('/dashboard', ensureAuthenticated, (req, res) => {
 
 app.get('/reset', (req, res) => {
     res.render('pages/reset');
+});
+
+// Optimization
+app.post('/optimization', ensureAuthenticated, (req, res) =>{
+    res.render('pages/optimization');
+    const {
+        stock1,
+        stock2,
+        stock3,
+        budget,
+        req_return,
+        start,
+        end,
+        period
+    } = req.body;
+    
+    var req = unirest("GET", 
+        "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-historical-data");
+
+    req.query({
+        "frequency": period,
+        "filter": "history",
+        "period1": "1546448400",
+        "period2": "1562086800",
+        "symbol": stock1
+    });
+
+    req.headers({
+        "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
+        "x-rapidapi-key": "3a6c5f19f5msh7f6a6a8ab33137dp1ff5c3jsnedbc4cb3b72b"
+    });
+    req.end(function (res) {
+        if (res.error) throw new Error(res.error);
+    
+        console.log(res.body[0]);
+    });
+
 });
 
 
